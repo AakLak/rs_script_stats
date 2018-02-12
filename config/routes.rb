7f671201a2d375a576f7137297299dd19b9b6d6d
Aakash Lakhotia <lakhotia.aakash@gmail.com>
2018-02-11 16:42:09 -0800

    db changes from testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :commits
-    resources :scripts
-    resources :users
-    resources :stats
-
-    root to: "commits#index"
-  end
 
   resources :scripts
-  
+
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v1 do
@@ -17,6 +9,15 @@ Rails.application.routes.draw do
       resources :commits
       resources :stats
     end
+  end
+
+  namespace :admin do
+    resources :commits
+    resources :scripts
+    resources :users
+    resources :stats
+
+    root to: "commits#index"
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
