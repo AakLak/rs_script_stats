dbb118b82b6d3ef74e7447c8e42d6e0691b4931f
Aakash Lakhotia <lakhotia.aakash@gmail.com>
2018-02-14 22:13:34 -0800

    add bootstrap, views for devise and homepage, distinct filter

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'jbuilder', '~> 2.5'
 # I added these
 gem 'devise'
 gem 'administrate'
+gem 'bootstrap', '~> 4.0.0'
+gem 'jquery-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 admin = User.create!(
-  email: 'gg@gg.com',
+  email: 'a@a.com',
   password: 'adminadmin',
   password_confirmation: 'adminadmin',
   admin: true
 )
+user = User.create!(
+  email: 'b@b.com',
+  password: 'useruser',
+  password_confirmation: 'useruser'
+)
+
+
 
 script = Script.create!(
   name: 'YWoodcutter',
@@ -21,26 +28,42 @@ script = Script.create!(
   user_id: admin.id
 )
 
-user = User.create!(
-  email: 'ghgh@ghgh.com',
-  password: 'useruser',
-  password_confirmation: 'useruser'
-)
+trees = %w(logs\ cut oaks\ cut willows\ cut teaks\ cut maples\ cut mahogoany\ cut yew\ cut magic\ cut)
+300.times do |i|
+  Commit.create!(
+    runtime: rand(240)+20,
+    user_id: user.id,
+    script_id: script.id,
+    created_at: DateTime.parse((Time.now - rand(1209600)).to_s)
+    )
+    Stat.create(
+      task: trees.sample,
+      amount: 28 * (rand(5)+1),
+      commit_id: i
+      )
+    if (rand(256) + 1) == 1
+      Stat.create(
+        task: "Nests",
+        amount: 1,
+        commit_id: i
+        )
+    end
+end
 
-user_commmit_1 = Commit.create!(
-  runtime: 75,
-  user_id: user.id,
-  script_id: script.id
-  )
+# user_commmit_1 = Commit.create!(
+#   runtime: 75,
+#   user_id: user.id,
+#   script_id: script.id
+#   )
 
-commit_1_stat_1 = Stat.create(
-  task: "Trees Chopped",
-  amount: 56,
-  commit_id: user_commmit_1.id
-  )
-
-commit_1_stat_2 = Stat.create(
-  task: "Nests Collected",
-  amount: 1,
-  commit_id: user_commmit_1.id
-  )
+# commit_1_stat_1 = Stat.create(
+#   task: "Trees Chopped",
+#   amount: 56,
+#   commit_id: user_commmit_1.id
+#   )
+#
+# commit_1_stat_2 = Stat.create(
+#   task: "Nests Collected",
+#   amount: 1,
+#   commit_id: user_commmit_1.id
+#   )
