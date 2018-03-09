0df626ae1068dd940d35fc8110a96884e79a0f40
Aakash Lakhotia <lakhotia.aakash@gmail.com>
2018-03-08 19:38:16 -0800

    comment out unnecessary/to-do specs in routing and controller

--- spec/requests/commits_spec.rb
+++ spec/requests/commits_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper'
-
-RSpec.describe "Commits", type: :request do
-  describe "GET /commits" do
-    it "works! (now write some real specs)" do
-      get commits_path
-      expect(response).to have_http_status(200)
-    end
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.describe "Commits", type: :request do
+#   describe "GET /commits" do
+#     it "works! (now write some real specs)" do
+#       get commits_path
+#       expect(response).to have_http_status(200)
+#     end
+#   end
+# end
