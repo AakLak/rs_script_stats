334d6a557dcadf0ae620661b1202a1a8f190348e
Aakash Lakhotia <lakhotia.aakash@gmail.com>
2018-03-21 01:50:33 -0700

    add test for Script.runtime

--- spec/models/script_spec.rb
+++ spec/models/script_spec.rb
@@ -11,4 +11,35 @@ RSpec.describe Script, type: :model do
   it { should validate_presence_of(:skill) }
   it { should validate_presence_of(:bot_for) }
   it { should validate_presence_of(:game_for) }
+
+  describe ".runtime" do
+    it 'returns script runtime' do
+      userID_1 = User.create!(
+        email: 'u1@u1.com',
+        password: 'useruser',
+        password_confirmation: 'useruser',
+      )
+
+      script1 = Script.create!(
+        name: 'YWoodcutter',
+        skill: 'Woodcutting',
+        bot_for: 'TRiBot',
+        game_for: 'Oldschool Runescape 07',
+        user_id: userID_1.id
+      )
+      Commit.create!(
+        runtime: 60,
+        user_id: userID_1.id,
+        script_id: script1.id,
+        created_at: DateTime.parse(Time.now.to_s)
+      )
+      Stat.create!(
+        task: 'Dummy Task',
+        amount: 1,
+        commit_id: 1
+      )
+
+      expect(script1.runtime).to eq 60.0
+    end
+  end
 end
