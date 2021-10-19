d5be2b15be2a349b011baf0279bb4a6cfb80ed5a
Aakash Lakhotia <lakhotia.aakash@gmail.com>
2021-10-19 00:18:18 -0700

    use dynamically defined attributes in Factorybot & cache bundler for Travis-CI

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
   factory :user do
-    email 'fafefeaectory@bot.com'
-    password 'useruser'
-    password_confirmation 'useruser'
+    email {'fafefeaectory@bot.com'}
+    password {'useruser'}
+    password_confirmation {'useruser'}
     id 2
   end
 
   # This will use the User class (Admin would have been guessed)
   factory :admin, class: User do
-    email 'aefae@adede.com'
-    password 'adminadmin'
-    password_confirmation 'adminadmin'
+    email {'aefae@adede.com'}
+    password {'adminadmin'}
+    password_confirmation {'adminadmin'}
     id 1
     admin true
   end
